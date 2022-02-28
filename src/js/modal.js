e371d8d8402ba3452d963186c400caa984be83e4
Margo <smsankova@gmail.com>
2022-02-28 16:12:22 +0200

    add desktop version

---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('[data-modal=consultation]').on('click', function (){
+        $('.overlay, #consultation').fadeIn('slow');
+    })
+
+    $('.modal__close').on('click', function (){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    })
+
+    $('.button_mini').each(function (i){
+        $(this).on('click', function (){
+            $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    })
+});
---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.next').addEventListener('click', () => slider.goTo('next'))
+
+document.querySelector('.prev').addEventListener('click', () => slider.goTo('prev'))
